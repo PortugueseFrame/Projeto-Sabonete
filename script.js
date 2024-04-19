1030278e9980ee9b53ef84b8d3e01dff14e0de05
Frame <104027762+PortugueseFrame@users.noreply.github.com>
2024-04-19 09:06:45 +0100

    Updated Fonts and Mobile support
    Added mobile support and updated the fonts and images to use less space and thus less heavy.

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleNavbarOpen() {
+
+    document.getElementById('miniNavbar').style.display = 'none';
+    document.getElementById('navbarExpanded').style.display = 'block';
+
+}
+
+function toggleNavbarClose() {
+
+    document.getElementById('miniNavbar').style.display = 'block';
+    document.getElementById('navbarExpanded').style.display = 'none';
+
+}
