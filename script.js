0ea2525918aeb91df235d686b3425a6b0e9736a8
Frame <104027762+PortugueseFrame@users.noreply.github.com>
2024-05-02 10:54:10 +0100

    Fixed Issues - 1.7*
    Added functionality to the toggleContent function where it closes active contents before opening new ones.
    *The last fixed issue should've been 1.6

--- script.js
+++ script.js
@@ -27,6 +27,16 @@ function scrollToBottom(timedelay=0) {
 document.querySelectorAll('.buttonToggle').forEach(function(button) {
     button.addEventListener('click', function() {
         var contentToggle = this.closest('.itemGroup').querySelector('.contentToggle');
+        var allContentToggles = document.querySelectorAll('.contentToggle');
+
+        allContentToggles.forEach(function(toggle) {
+            if (toggle !== contentToggle && toggle.classList.contains("active")) {
+                toggle.classList.remove("active");
+                toggle.style.display = "none";
+                var correspondingButton = toggle.closest('.itemGroup').querySelector('.buttonToggle');
+                correspondingButton.textContent = "+";
+            }
+        });
         
         if (contentToggle) {
             contentToggle.classList.toggle("active");
@@ -35,6 +45,8 @@ document.querySelectorAll('.buttonToggle').forEach(function(button) {
         }
     });
 });
+
+
 
 function toggleForm() {
     var form = document.getElementsByTagName('form')[0];
